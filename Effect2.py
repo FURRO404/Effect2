bb8f1b14ce82aa688acde64f701c0f9208ed1775
NotSoToothless <67082114+FURRO404@users.noreply.github.com>
2021-03-28 14:09:23 -0700

    Update Effect2.py

--- Effect2.py
+++ Effect2.py
@@ -10,14 +10,15 @@ def listconv(line):
 line = []
 
 while True:
-    double = random.randint(0, 1)
+    double = random.randint(0, 2) #Get random number to choose for a double character instance.
     
     if double == 0:
-        mix = random.randint(0, 100)
+        mix = random.randint(0, 100) #100 is maximum distance for line length.
+        
         for i in range(0, mix):
-            line.append(" ")
+            line.append(" ") 
 
-        choices = [".", "/", ">", "#", "@", "*", "%", "<"]
+        choices = [".", "/", ">", "#", "@", "*", "%", "<"] #List of choices for character to be.
         choice = random.choice(choices)
         line.append(choice)
         
@@ -25,13 +26,29 @@ while True:
         time.sleep(0.001)
         line.clear()
 
-    if double == 1:
+    elif double == 1:
         for i in range(0, 2):
-            mix = random.randint(0, 50)
+            mix = random.randint(0, 50) #Lowered to 50 so line can never exceed 100.
+            
             for i in range(0, mix):
                 line.append(" ")
                 
-            choices = [".", "/", ">", "#", "@", "*", "%", "<"]
+            choices = [".", "/", ">", "#", "@", "*", "%", "<"] #List of choices for character to be.
+            choice = random.choice(choices)
+            line.append(choice)
+            
+        print(listconv(line))
+        time.sleep(0.001)
+        line.clear()
+        
+    elif double == 2:
+        for i in range(0, 3):
+            mix = random.randint(0, 33) #Lowered to 33 so line can never exceed 100.
+            
+            for i in range(0, mix):
+                line.append(" ")
+                
+            choices = [".", "/", ">", "#", "@", "*", "%", "<"] #List of choices for character to be.
             choice = random.choice(choices)
             line.append(choice)
             
